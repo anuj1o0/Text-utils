44256208b1b1e6aa580cbacce75cde4a933a3924
Anuj Srivastava <118746163+anuj1o0@users.noreply.github.com>
2023-08-20 20:17:54 +0530

    Some Updates

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import About from "./components/About";
 
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import React, { useState } from 'react'
-import Alert from './components/Alert';
-import About from './components/About';
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  const [alert, setAlert]=useState(null);
+  const [alert, setAlert] = useState(null);
   return (
     <>
-    <Router>
-    <Navbar title = "TextUtils" />
-    <Alert Alert="This is alert"/>
-    <div className="container">
-    <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
+      {/* <Router> */}
+        <Navbar title="TextUtils" />
+        <Alert Alert="This is alert" />
+        <div className="container">
+          {/* <Switch>
+            <Route path="/about">
+              <About />
+            </Route> */}
 
-          <Route path="/">
-          <TextForm heading="Enter the text to analyze"/>
-          </Route>
-    </Switch>
-    
-    </div>
-    </Router>
+            {/* <Route path="/"> */}
+              <TextForm heading="Enter the text to analyze" />
+            {/* </Route>
+          </Switch> */}
+        </div>
+      {/* </Router> */}
     </>
   );
 }
